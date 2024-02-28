3f433e125577aba6c8b4f1c9e9625026235c30cd
squoshi <95097230+squoshi@users.noreply.github.com>
2024-02-27 18:29:41 -0600

    Delete build directory

--- build/tmp/.cache/expanded/zip_21234ec023791899cefdd379b29fa3fc/net/minecraft/world/ContainerListener.java
+++
@@ -1,8 +0,0 @@
-package net.minecraft.world;
-
-public interface ContainerListener {
-   /**
-    * Called by {@code InventoryBasic.onInventoryChanged()} on an array that is never filled.
-    */
-   void containerChanged(Container pContainer);
-}
