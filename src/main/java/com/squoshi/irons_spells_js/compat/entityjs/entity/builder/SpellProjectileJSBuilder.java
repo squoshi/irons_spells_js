24c5ea4af8ea3e65570cf28f47b4a65040d65288
squoshi <95097230+squoshi@users.noreply.github.com>
2024-03-19 09:35:38 -0500

    More EntityJS stuffs

--- src/main/java/com/squoshi/irons_spells_js/compat/entityjs/entity/builder/SpellProjectileJSBuilder.java
+++ src/main/java/com/squoshi/irons_spells_js/compat/entityjs/entity/builder/SpellProjectileJSBuilder.java
@@ -1,36 +1,19 @@
 package com.squoshi.irons_spells_js.compat.entityjs.entity.builder;
 
 import com.squoshi.irons_spells_js.compat.entityjs.entity.SpellProjectileJS;
-import dev.latvian.mods.kubejs.registry.RegistryInfo;
-import dev.latvian.mods.kubejs.typings.Generics;
-import dev.latvian.mods.kubejs.typings.Info;
 import net.liopyu.entityjs.builders.ProjectileEntityBuilder;
-import net.liopyu.entityjs.item.ProjectileItemBuilder;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.Level;
 
 import java.util.function.Consumer;
 
 public class SpellProjectileJSBuilder extends ProjectileEntityBuilder<SpellProjectileJS> {
-    public transient Consumer<SpellProjectileJS.EntityProjectileWeaponContext> damageEntity;
+    public transient Consumer<SpellProjectileJS.OnAntiMagicContext> onAntiMagic;
+
     public SpellProjectileJSBuilder(ResourceLocation i) {
         super(i);
     }
-    @Info(value = """
-            Sets the logic for damaging the entities this spell hits.
-            
-            Example usage:
-            ```javascript
-            builder.damageEntity(context => {
-            //Damage the entity with the provided context.
-            });
-            ```
-            """)
-    public SpellProjectileJSBuilder damageEntity(Consumer<SpellProjectileJS.EntityProjectileWeaponContext> f) {
-        this.damageEntity = f;
-        return this;
-    }
+
     @Override
     public EntityType.EntityFactory<SpellProjectileJS> factory() {
         return (type, level) -> new SpellProjectileJS(this, type, level);
