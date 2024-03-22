a2616ea701d0597e1c09e83ce1e991dfc1ba9d38
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2024-03-22 05:37:35 -0300

    added Pseudo to make mixin optional

--- src/main/java/com/squoshi/irons_spells_js/mixin/NameResolverMixin.java
+++ src/main/java/com/squoshi/irons_spells_js/mixin/NameResolverMixin.java
@@ -3,12 +3,14 @@ package com.squoshi.irons_spells_js.mixin;
 import com.probejs.compiler.formatter.NameResolver;
 import com.squoshi.irons_spells_js.util.ISSKJSUtils;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.List;
 
+@Pseudo
 @Mixin(value = NameResolver.class, remap = false)
 public class NameResolverMixin {
     @Inject(method = "init", at = @At(value = "TAIL"))
