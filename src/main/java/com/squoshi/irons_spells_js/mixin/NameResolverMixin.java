6d353d84a1cd2d4730d2c4fd27fafff13e63ad54
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2024-04-06 02:31:56 -0300

    added School and Spells to Probe

--- src/main/java/com/squoshi/irons_spells_js/mixin/NameResolverMixin.java
+++ src/main/java/com/squoshi/irons_spells_js/mixin/NameResolverMixin.java
@@ -1,7 +1,12 @@
 package com.squoshi.irons_spells_js.mixin;
 
 import com.probejs.compiler.formatter.NameResolver;
+import com.probejs.compiler.formatter.SpecialTypes;
 import com.squoshi.irons_spells_js.util.ISSKJSUtils;
+import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
+import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.spells.AbstractSpell;
+import io.redspace.ironsspellbooks.api.spells.SchoolType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,5 +22,9 @@ public class NameResolverMixin {
     private static void kjs_irons_spells$injectTypes(CallbackInfo ci){
         NameResolver.putSpecialAssignments(ISSKJSUtils.AttributeHolder.class, () -> List.of("Special.Attribute"));
         NameResolver.putSpecialAssignments(ISSKJSUtils.SoundEventHolder.class, () -> List.of("Special.SoundEvent"));
+        NameResolver.putSpecialAssignments(ISSKJSUtils.SchoolHolder.class, () -> List.of("Special.School"));
+        NameResolver.putSpecialAssignments(ISSKJSUtils.SpellHolder.class, () -> List.of("Special.Spells"));
+        SpecialTypes.assignRegistry(AbstractSpell.class, SpellRegistry.SPELL_REGISTRY_KEY);
+        SpecialTypes.assignRegistry(SchoolType.class, SchoolRegistry.SCHOOL_REGISTRY_KEY);
     }
 }
