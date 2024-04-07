df87e36077fc3e6d4b7dbcf95ae200f77c8a3d9e
squoshi <95097230+squoshi@users.noreply.github.com>
2024-04-07 02:04:47 -0500

    Fix EntityJS version, add `getMagicData` using PlayerMixin

---
+++ src/main/java/com/squoshi/irons_spells_js/mixin/PlayerMixin.java
@@ -0,0 +1,13 @@
+package com.squoshi.irons_spells_js.mixin;
+
+import dev.latvian.mods.rhino.util.RemapPrefixForJS;
+import io.redspace.ironsspellbooks.api.magic.MagicData;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Player.class)
+public class PlayerMixin {
+    public MagicData kjs$getMagicData() {
+        return MagicData.getPlayerMagicData((Player) (Object) this);
+    }
+}
