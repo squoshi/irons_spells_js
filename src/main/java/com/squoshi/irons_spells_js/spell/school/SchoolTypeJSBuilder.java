e78e1ad1f99e6fbdbda13fe51b7ccc834cc4ac7d
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2024-04-25 01:02:12 -0300

    Focus now accept items and auto add to focus tag

--- src/main/java/com/squoshi/irons_spells_js/spell/school/SchoolTypeJSBuilder.java
+++ src/main/java/com/squoshi/irons_spells_js/spell/school/SchoolTypeJSBuilder.java
@@ -1,20 +1,27 @@
 package com.squoshi.irons_spells_js.spell.school;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import com.squoshi.irons_spells_js.IronsSpellsJSPlugin;
 import com.squoshi.irons_spells_js.util.ISSKJSUtils;
+import dev.latvian.mods.kubejs.generator.DataJsonGenerator;
 import dev.latvian.mods.kubejs.registry.BuilderBase;
 import dev.latvian.mods.kubejs.registry.RegistryInfo;
+import dev.latvian.mods.kubejs.util.ConsoleJS;
 import io.redspace.ironsspellbooks.api.spells.SchoolType;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @SuppressWarnings("unused")
@@ -25,14 +32,20 @@ public class SchoolTypeJSBuilder extends BuilderBase<SchoolType> {
     public transient LazyOptional<Attribute> powerAttribute;
     public transient LazyOptional<Attribute> resistanceAttribute;
     public transient LazyOptional<SoundEvent> defaultCastSound;
+    public transient List<Item> focusItems = new ArrayList<>();
 
     public SchoolTypeJSBuilder(ResourceLocation i) {
         super(i);
         this.schoolResource = i;
+        this.focus = ItemTags.create(new ResourceLocation(this.schoolResource.getNamespace(), this.schoolResource.getPath() + "_focus"));
     }
 
-    public SchoolTypeJSBuilder setFocus(ResourceLocation focus) {
-        this.focus = TagKey.create(ResourceKey.createRegistryKey(new ResourceLocation("minecraft:item")), focus);
+    public SchoolTypeJSBuilder setFocus(Item ...focusItems) {
+        for (var item : focusItems){
+            if (Items.AIR == item){
+                ConsoleJS.STARTUP.error("Tried to add an invalid item to IronSpells School Focus");
+            } else this.focusItems.add(item);
+        }
         return this;
     }
 
@@ -71,5 +84,28 @@ public class SchoolTypeJSBuilder extends BuilderBase<SchoolType> {
                 this.resistanceAttribute,
                 this.defaultCastSound
         );
+    }
+
+    @Override
+    public void generateDataJsons(DataJsonGenerator generator) {
+        var focusJson = new JsonObject();
+        focusJson.addProperty("replace", false);
+        var focusArray = new JsonArray();
+        focusArray.add("#" + this.focus.location());
+        focusJson.add("values", focusArray);
+        var target = new ResourceLocation("irons_spellbooks:tags/items/school_focus");
+        generator.json(target, focusJson);
+
+        if (!this.focusItems.isEmpty()){
+            var itemsJson = new JsonObject();
+            itemsJson.addProperty("replace", false);
+            var itemsArray = new JsonArray();
+            for (var item : this.focusItems) {
+                itemsArray.add(Objects.requireNonNull(ForgeRegistries.ITEMS.getKey(item)).toString());
+            }
+            itemsJson.add("values", itemsArray);
+            var itemsTarget = new ResourceLocation(this.focus.location().getNamespace() + ":tags/items/"+ this.focus.location().getPath());
+            generator.json(itemsTarget, itemsJson);
+        }
     }
 }
