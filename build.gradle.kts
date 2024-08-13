46c6a5ff03f8857f98d9f65bd802b014e82d4801
pietro-lopes <97140255+pietro-lopes@users.noreply.github.com>
2024-08-13 06:05:28 -0300

    ported gradle to kotlin script

---
+++ build.gradle.kts
@@ -0,0 +1,137 @@
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
+plugins {
+    java
+    idea
+    `maven-publish`
+    id("net.neoforged.moddev") version "2.0.10-beta"
+}
+
+val minecraftVersion: String by project
+val minecraftVersionRange: String by project
+val neoVersion: String by project
+val neoVersionRange: String by project
+val loaderVersionRange: String by project
+
+val modId: String by project
+val modName: String by project
+val modLicense: String by project
+val modVersion: String by project
+val modGroupId: String by project
+val modAuthors: String by project
+val modDescription: String by project
+
+val ironsSpellbooksVersion: String by project
+val ironsSpellbooksFileId: String by project
+val kubejsVersion: String by project
+
+repositories {
+    mavenLocal()
+    maven("https://maven.saps.dev/minecraft")
+    maven("https://www.cursemaven.com")
+    flatDir {
+        dir("libs")
+    }
+}
+
+base {
+    archivesName = modId
+    version = modVersion
+    group = modGroupId
+}
+
+neoForge {
+    version = neoVersion
+    validateAccessTransformers = true
+
+    runs {
+        register("gameTestServer") {
+            server()
+            systemProperty("neoforge.enabledGameTestNamespaces", modId)
+        }
+        register("client") {
+            client()
+        }
+        register("data") {
+            data()
+        }
+        register("server") {
+            server()
+        }
+    }
+
+    mods {
+        register(modId) {
+            sourceSet(sourceSets.main.get())
+        }
+    }
+}
+
+java.toolchain.languageVersion = JavaLanguageVersion.of(21)
+
+dependencies {
+    implementation(accessTransformers(interfaceInjectionData("dev.latvian.mods:kubejs-neoforge:$kubejsVersion")!!)!!)
+
+    implementation("curse.maven:irons-spells-n-spellbooks-855414:$ironsSpellbooksFileId")
+    runtimeOnly("curse.maven:adorned-1036809:5546365") // curios-neoforge-9.0.5+1.21.0.jar
+    runtimeOnly("curse.maven:caelus-308989:5442975") // caelus-neoforge-7.0.0+1.21.jar
+    runtimeOnly("curse.maven:geckolib-388172:5605715") // geckolib-neoforge-1.21-4.5.8.jar
+
+    runtimeOnly("curse.maven:emi-580555:5619579")
+    runtimeOnly("curse.maven:jade-324717:5591256")
+//    runtimeOnly("curse.maven:probejs-585406:5536459")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("net.neoforged:testframework:$neoVersion")
+}
+
+tasks {
+    processResources {
+        val replaceProperties = mapOf(
+            "minecraft_version" to minecraftVersion,
+            "minecraft_version_range" to minecraftVersionRange,
+            "neo_version" to neoVersion,
+            "neo_version_range" to neoVersionRange,
+            "loader_version_range" to loaderVersionRange,
+            "mod_id" to modId,
+            "mod_name" to modName,
+            "mod_license" to modLicense,
+            "mod_version" to modVersion,
+            "mod_authors" to modAuthors,
+            "mod_description" to modDescription,
+            "irons_spellbooks_version" to ironsSpellbooksVersion,
+            "kubejs_version" to kubejsVersion
+        )
+
+        inputs.properties(replaceProperties)
+        filesMatching(listOf("META-INF/neoforge.mods.toml")) {
+            expand(replaceProperties)
+        }
+    }
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("mavenJava") {
+            from(components.getByName("java"))
+        }
+    }
+    repositories {
+        maven("file://$projectDir/repo")
+    }
+}
+
+idea {
+    module {
+        isDownloadSources = true
+        isDownloadJavadoc = true
+    }
+    project {
+        jdkName = java.sourceCompatibility.toString()
+        languageLevel = IdeaLanguageLevel(java.sourceCompatibility.toString())
+    }
+}
