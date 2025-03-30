b3b0114240d40ae2fe141cafc322dcd457443a50
squoshi <95097230+squoshi@users.noreply.github.com>
2025-03-30 00:22:26 -0500

    Patch

--- src/main/java/com/squoshi/irons_spells_js/IronsSpellsJSPlugin.java
+++ src/main/java/com/squoshi/irons_spells_js/IronsSpellsJSPlugin.java
@@ -15,7 +15,19 @@ import dev.latvian.mods.kubejs.registry.BuilderTypeRegistry;
 import dev.latvian.mods.kubejs.script.BindingRegistry;
 import io.redspace.ironsspellbooks.api.registry.SchoolRegistry;
 import io.redspace.ironsspellbooks.api.registry.SpellRegistry;
+import io.redspace.ironsspellbooks.api.spells.CastType;
+import io.redspace.ironsspellbooks.api.spells.SpellData;
+import io.redspace.ironsspellbooks.api.spells.SpellRarity;
+import io.redspace.ironsspellbooks.api.util.AnimationHolder;
+import io.redspace.ironsspellbooks.api.util.Utils;
+import io.redspace.ironsspellbooks.capabilities.magic.TargetEntityCastData;
+import io.redspace.ironsspellbooks.entity.mobs.goals.*;
+import io.redspace.ironsspellbooks.registries.PotionRegistry;
+import io.redspace.ironsspellbooks.util.ParticleHelper;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.alchemy.Potions;
 
 public final class IronsSpellsJSPlugin implements KubeJSPlugin {
 
@@ -36,29 +48,28 @@ public final class IronsSpellsJSPlugin implements KubeJSPlugin {
 
 	@Override
 	public void registerBindings(BindingRegistry event) {
-//		event.add("SpellRarity", SpellRarity.class);
-//		event.add("SchoolRegistry", SchoolRegistry.class);
-//		event.add("CastType", CastType.class);
-//		event.add("IronsSpellsParticleHelper", ParticleHelper.class);
-//		event.add("SpellRegistry", SpellRegistry.class);
-//		event.add("ItemTags", ItemTags.class);
-//		event.add("Player", Player.class);
-//		event.add("SpellData", SpellData.class);
+		event.add("SpellRarity", SpellRarity.class);
+		event.add("SchoolRegistry", SchoolRegistry.class);
+		event.add("CastType", CastType.class);
+		event.add("IronsSpellsParticleHelper", ParticleHelper.class);
+		event.add("SpellRegistry", SpellRegistry.class);
+		event.add("ItemTags", ItemTags.class);
+		event.add("Player", Player.class);
+		event.add("SpellData", SpellData.class);
 		event.add("Spell", AbstractSpellWrapper.class);
-//		event.add("ISSAnimationHolder", AnimationHolder.class);
-//		event.add("ISSUpdateClient", UpdateClient.class);
-//		event.add("ISSUtils", Utils.class);
-//		event.add("TargetEntityCastData", TargetEntityCastData.class);
-//		event.add("Potions", Potions.class);
-//		event.add("ISSPotionRegistry", PotionRegistry.class);
+		event.add("ISSAnimationHolder", AnimationHolder.class);
+		event.add("ISSUtils", Utils.class);
+		event.add("TargetEntityCastData", TargetEntityCastData.class);
+		event.add("Potions", Potions.class);
+		event.add("ISSPotionRegistry", PotionRegistry.class);
 //		event.add("AlchemistCauldronRecipeBuilder", AlchemistCauldronKubeJSRecipes.AlchemistCauldronRecipeBuilder.class);
-//		event.add("WizardAttackGoal", WizardAttackGoal.class);
-//		event.add("WarlockAttackGoal", WarlockAttackGoal.class);
-//		event.add("WizardRecoverGoal", WizardRecoverGoal.class);
-//		event.add("WizardSupportGoal", WizardSupportGoal.class);
-//		event.add("SpellBarrageGoal", SpellBarrageGoal.class);
-//		event.add("GustDefenseGoal", GustDefenseGoal.class);
-//		event.add("WispAttackGoal", WispAttackGoal.class);
+		event.add("WizardAttackGoal", WizardAttackGoal.class);
+		event.add("WarlockAttackGoal", WarlockAttackGoal.class);
+		event.add("WizardRecoverGoal", WizardRecoverGoal.class);
+		event.add("WizardSupportGoal", WizardSupportGoal.class);
+		event.add("SpellBarrageGoal", SpellBarrageGoal.class);
+		event.add("GustDefenseGoal", GustDefenseGoal.class);
+		event.add("WispAttackGoal", WispAttackGoal.class);
 	}
 
 	@Override
